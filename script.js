387b724e859ec64d4386a35d0c4cbe7a5aa49a9e
TR742 <Tyrich1442@gmail.com>
2022-04-06 19:12:00 -0700

    save to local storage working

---
+++ script.js
@@ -0,0 +1,25 @@
+// Adding the current date into the header
+let date = $('#currentDay');
+let now = moment();
+date.text(now.format('dddd MMMM Do YYYY'));
+
+// Variables for time of day
+let nine = $('#nine');
+let ten = $('#ten');
+let eleven = $('#eleven');
+let twelve = $('#twelve');
+let one = $('#one');
+let two = $('#two');
+let three = $('#three');
+let four = $('#four');
+let five = $('#five');
+
+// Save button function to save userInput to local storage, need to add getItem function working
+let saveBtn = $(".saveBtn");
+
+saveBtn.on("click", function() {
+    let hour = $(this).siblings(".hour").text();
+    let userInput = $(this).siblings(".description").val();
+
+    localStorage.setItem(hour, userInput);
+});
