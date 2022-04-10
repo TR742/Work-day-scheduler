04d43a03dcb038bbaab7c9cc1b885b58565342c3
TR742 <Tyrich1442@gmail.com>
2022-04-10 10:33:10 -0700

    Local storage setItem/getItem working

--- script.js
+++ script.js
@@ -15,10 +15,20 @@ let four = $('#four');
 let five = $('#five');
 
 // Save button function to save userInput to local storage, need to add getItem function working
-let saveBtn = $(".saveBtn");
+let saveBtn = $(".saveBtn") 
 
-saveBtn.on("click", function() {
-    let hour = $(this).siblings(".hour").text();
-    let userInput = $(this).siblings(".description").val();
-    localStorage.setItem(hour, userInput);
-});
+saveBtn.on('click', function () {
+    let inputValue=$(this).siblings('.description').val()
+    let id=$(this).parent().attr('id')
+    localStorage.setItem(id,inputValue)
+    });
+
+$('#nine .description').val(localStorage.getItem('nine'));
+$('#ten .description').val(localStorage.getItem('ten'));
+$('#eleven .description').val(localStorage.getItem('eleven'));
+$('#twelve .description').val(localStorage.getItem('twelve'));
+$('#one .description').val(localStorage.getItem('one'));
+$('#two .description').val(localStorage.getItem('two'));
+$('#three .description').val(localStorage.getItem('three'));
+$('#four .description').val(localStorage.getItem('four'));
+$('#five .description').val(localStorage.getItem('five'));
